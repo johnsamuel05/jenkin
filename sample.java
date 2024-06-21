1d0daf531bd2907d1379abe08d298f19adcad28c
Nandhini0307 <60594778+Nandhini0307@users.noreply.github.com>
2024-06-21 07:35:06 +0530

    sample.java

---
+++ sample.java
@@ -0,0 +1,8 @@
+public class Hello{
+  public static void main(String[] args){
+    System.out.println("welcome");
+    System.out.println("to");
+    System.out.println("Devops");
+    System.out.println("class");
+  }
+}
